6cd09b1eb12e3433cb684dcb3c49c8b10a3c12e9
robert.alexandersson <robert.alexandersson@25a7e968-e0cf-11de-a6a9-d5aeed48c2a6>
2010-03-25 23:38:40 +0000

    Added some testcases

---
+++ trunk/src/test/java/test/org/alex/springext/scope/ThreadScopedBean.java
@@ -0,0 +1,19 @@
+package test.org.alex.springext.scope;
+
+import org.alex.springext.scope.ScopeType;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Robert
+ * Date: 2010-mar-26
+ * Time: 00:17:23
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+@Component
+@Scope(value = ScopeType.THREAD)
+public class ThreadScopedBean {
+}
