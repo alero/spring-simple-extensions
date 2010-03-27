0ea76fe7c75cd46075cf26ba17fc4afa0d585e13
robert.alexandersson@gmail.com <robert.alexandersson@gmail.com@25a7e968-e0cf-11de-a6a9-d5aeed48c2a6>
2010-03-27 00:21:34 +0000

    Added TDD (test driven development) support to extensions. This made it possible to activate the JDBC tests

--- trunk/src/test/java/test/org/alex/springext/jdbc/TestJdbcInsertUpdater.java
+++ trunk/src/test/java/test/org/alex/springext/jdbc/TestJdbcInsertUpdater.java
@@ -2,10 +2,16 @@ package test.org.alex.springext.jdbc;
 
 import org.alex.springext.jdbc.ExtendedSimpleJdbcTemplate;
 import org.alex.springext.jdbc.InsertUpdater;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.sql.DataSource;
 import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,19 +21,29 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        locations = {"classpath:org/alex/springext/tdd/datasource-spring-config.xml"}
+)
 public class TestJdbcInsertUpdater {
 
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    @Ignore // i need the hipersonic db setup first
-    public void testJdbc(){
-        ExtendedSimpleJdbcTemplate extendedSimpleJdbcTemplate = new ExtendedSimpleJdbcTemplate(null);
+    public void testJdbcSelect(){
+        ExtendedSimpleJdbcTemplate extendedSimpleJdbcTemplate = new ExtendedSimpleJdbcTemplate(dataSource);
         InsertUpdater insertUpdater = extendedSimpleJdbcTemplate.createUpdateTemplate("person");
-        insertUpdater.field("date", new Date());
+        insertUpdater.field("id", 1);
+        insertUpdater.field("name","John Doe");
+        insertUpdater.field("email", "john@doe.com");
+        insertUpdater.field("createdate", new Date());
         extendedSimpleJdbcTemplate.update(insertUpdater);
 
-    }
+        int id = extendedSimpleJdbcTemplate.queryForInt("select id from person where id = ?", 1);
+        assertEquals(1, id); 
 
+    }
 
 }
